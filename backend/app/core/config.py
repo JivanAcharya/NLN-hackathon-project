23e544d09b2db24e0f080a9b4707cd358985c97a
Jivan Acharya <i1001730@verisk.com>
2026-03-29 22:11:53 +0545

    update env read

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,15 +1,15 @@
-from dotenv import load_dotenv, dotenv_values
+import os
+from dotenv import load_dotenv
 # from pydantic_settings import BaseSettings
 
 class Settings:
     def __init__(self):
         load_dotenv()
-        self.env_vars = dotenv_values()
 
-        self.database_url = self.env_vars.get("DATABASE_URL")
-        self.hashing_algorithm = self.env_vars.get("HASHING_ALGORITHM")
-        self.secret_key = self.env_vars.get("SECRET_KEY")
-        self.allowed_origins: list = [o.strip() for o in self.env_vars.get("ALLOWED_ORIGINS", "http://localhost:5173").split(",") if o.strip()]
+        self.database_url = os.getenv("DATABASE_URL")
+        self.hashing_algorithm = os.getenv("HASHING_ALGORITHM")
+        self.secret_key = os.getenv("SECRET_KEY")
+        self.allowed_origins: list = [o.strip() for o in os.getenv("ALLOWED_ORIGINS", "http://localhost:5173").split(",") if o.strip()]
 
         self.access_token_expire_time: int  = 60
         self.refresh_token_expire_time: int = 60 * 24 * 7
